74118b1484f8afde277bcf170305be7bdb62d36d
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-07 16:12:00 -0500

    Added error code and some comments.

--- Develop/server.js
+++ Develop/server.js
@@ -4,28 +4,25 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Insert global middleware here
-// app.use(clog);
-
-// Allow for static files to be accessed
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
   res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
+//Get request to go to the notes page
 app.get("/notes", (req, res) => {
   console.log("Is this working?")
   res.sendFile(path.join(__dirname, "public/notes.html"))
-})
+});
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+// Post
 
-// Set up extra routing files
-// app.use('/api', require('./routes/api/index.js'));
-// app.use('/', require('./routes/html/index.js'));
+// Delete (optional)
 
+
+//Code for Error Page
+app.get("*", (req, res) => res.send("File Not Found"));
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
